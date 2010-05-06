8adf8685a48a6cc5bc3791789d6ee242785b4c54
spalladino <devnull@localhost>
2010-05-06 13:35:28 -0300

    Fixes in path detector

--- pcp.tests/src/pcp/tests/algorithms/ComponentPathDetectorFixture.java
+++ pcp.tests/src/pcp/tests/algorithms/ComponentPathDetectorFixture.java
@@ -38,7 +38,7 @@ public class ComponentPathDetectorFixture {
 	}
 	
 	@Test
-	public void testK5WithHole() throws AlgorithmException {
+	public void testK5() throws AlgorithmException {
 		
 		builder.createNodes(5);
 		
@@ -59,13 +59,39 @@ public class ComponentPathDetectorFixture {
 		test();
 	}
 	
+	@Test
+	public void testK5PlusPath() throws AlgorithmException {
+		
+		builder.createNodes(7);
+		
+		builder.addEdge(0, 1)
+			.addEdge(1, 2)
+			.addEdge(2, 3)
+			.addEdge(3, 4)
+			.addEdge(4, 0)
+			.addEdge(4, 5)
+			.addEdge(5, 6);
+	
+		colors = 3;
+		data = new StubModelData(7).withWs(0.8, 1.0, 1.0)
+			.withXs(0, 0.4, 0.5, 0.2)
+			.withXs(1, 0.4, 0.5, 0.4)
+			.withXs(2, 0.4, 0.5, 0.9)
+			.withXs(3, 0.4, 0.5, 0.1)
+			.withXs(4, 0.4, 0.5, 0.1)
+			.withXs(5, 0.4, 0.5, 0.9)
+			.withXs(6, 0.4, 0.5, 0.6);
+	
+		test();
+	}
+	
 	private void test() {
 		PartitionedGraph graph = builder.getGraph();
 		Model model = new Model(graph, colors);
 		Iteration iter = new Iteration(model, data, cuts);
 		ComponentPathDetector detector = new ComponentPathDetector(iter);
 		detector.run();
-		
+		System.out.println("\nGraph with " + graph.N() + " nodes and " + graph.E() + " edges.");
 		cuts.printIneqs(System.out);
 	}
 	
