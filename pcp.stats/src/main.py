bb7dfe3390d5851f776c2c669fcba486b188c2eb
spalladino <devnull@localhost>
2010-05-18 11:24:36 -0300

    Informs slides initial

--- pcp.stats/src/main.py
+++ pcp.stats/src/main.py
@@ -8,8 +8,10 @@ if __name__ == '__main__':
     #p.graphprops("graph.edges", "solution.time", "edges-time.png")
     #p.graphprops("graph.partitions", "solution.time", "parts-time.png")
     #p.graph("graph.nodes", "solution.time", series= ["holes.maxPerColor"], fname= "nodestime.png")
-    p.graph("graph.nodes", "solution.chi", 
-            series= ["strategy.coloring",
-                     "dsatur.colorAdjPartitions",
+    p.graph("graph.nodes", "solution.time", 
+            series= ["path.enabled", 
+                    "clique.enabled",
+                    "blockColor.enabled",
+                    "holes.enabled",
                      ])
     #p.graph("graph.nodes", metrics.cutcount("Holes"), series= ["holes.maxPerColor"])
