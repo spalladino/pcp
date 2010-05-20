e66ef64ca7b9b8a370c00ec8d9dcf16a9ffb8c55
spalladino <devnull@localhost>
2010-05-19 21:13:02 -0300

    New run with initial heur

--- pcp.runner/src/main.py
+++ pcp.runner/src/main.py
@@ -4,59 +4,66 @@ if __name__ == '__main__':
     #if not resume():
     newrun([
                 {
-                    "path.enabled": 'false', 
-                    "clique.enabled": 'false',
-                    "blockColor.enabled": 'false',
-                    "gprime.enabled": 'false',
-                    "holes.enabled": 'false',
-                    "branch.prios.enabled": 'false',
-                    "primal.enabled": 'false',
-                }, 
-                {
-                    "path.enabled": 'false', 
-                    "clique.enabled": 'true',
-                    "blockColor.enabled": 'true',
-                    "gprime.enabled": 'false',
-                    "holes.enabled": 'false',
-                    "branch.prios.enabled": 'false',
-                    "primal.enabled": 'false',
+                    'cuts.minCliques': '0',
+                    'iterations.root.max': '5',
+                    'primal.enabled': 'true',
+                    'pruning.remaining': '0'
                 },
                 {
-                    "path.enabled": 'false', 
-                    "clique.enabled": 'false',
-                    "blockColor.enabled": 'false',
-                    "gprime.enabled": 'false',
-                    "holes.enabled": 'false',
-                    "branch.prios.enabled": 'true',
-                    "primal.enabled": 'true',
-                }, 
+                    'cuts.minCliques': '50',
+                    'iterations.root.max': '5',
+                    'primal.enabled': 'true',
+                    'pruning.remaining': '0'
+                },
                 {
-                    "path.enabled": 'false', 
-                    "clique.enabled": 'false',
-                    "blockColor.enabled": 'false',
-                    "gprime.enabled": 'false',
-                    "holes.enabled": 'false',
-                    "branch.prios.enabled": 'true',
-                    "primal.enabled": 'false',
-                }, 
-
+                    'cuts.minCliques': '50',
+                    'iterations.root.max': '20',
+                    'primal.enabled': 'true',
+                    'pruning.remaining': '0'
+                },
                 {
-                    "path.enabled": 'false', 
-                    "clique.enabled": 'true',
-                    "blockColor.enabled": 'true',
-                    "gprime.enabled": 'false',
-                    "holes.enabled": 'false',
-                    "branch.prios.enabled": 'true',
-                    "primal.enabled": 'true',
-                }, 
-
+                    'cuts.minCliques': '50',
+                    'iterations.root.max': '20',
+                    'primal.enabled': 'true',
+                    'pruning.remaining': '15'
+                },
+                {
+                    'cuts.minCliques': '50',
+                    'iterations.root.max': '20',
+                    'primal.enabled': 'true',
+                    'pruning.remaining': '25'
+                },
+                {
+                    'cuts.minCliques': '50',
+                    'iterations.root.max': '20',
+                    'primal.enabled': 'false',
+                    'pruning.remaining': '15'
+                    
+                },
+                {
+                    'cuts.minCliques': '0',
+                    'iterations.root.max': '20',
+                    'primal.enabled': 'false',
+                    'pruning.remaining': '0'
+                },
+                {
+                    'cuts.minCliques': '0',
+                    'iterations.root.max': '20',
+                    'primal.enabled': 'false',
+                    'pruning.remaining': '15'
+                    
+                },    
+ 
  
             ], 
             
             files = [ 
-                        "rand30.in",
-                        "rand40.in",
-                        "rand50.in",
+                        "rand100.in",
+                        "rand110.in",
+                        "rand120.in",
+                        "rand130.in",
+                        "rand140.in",
+                        "rand150.in",
                     ],
                     
             dirs = [
