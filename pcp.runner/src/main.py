c4bf1c66ef4fff8e939c74b8778af7205e76c931
spalladino <devnull@localhost>
2010-06-24 17:47:50 -0300

    Add runs to runset

--- pcp.runner/src/main.py
+++ pcp.runner/src/main.py
@@ -11,55 +11,17 @@ if __name__ == '__main__':
     files = fetcher.Fetcher(datadir).fetch_files('benchdens', 'e0(2|4|6|8)n100\\.00[\\d]\\.in') + fetcher.Fetcher(datadir).fetch_files('holme', 'n100d0(1|2|3|4)\\.00[\\d]\\.in')   
     print 'Processing: ', files
    
-    newrun([
+    Fixture().newrun(
+            [
                 {
-                    'strategy.partition': 'PaintAtLeastOne',
+                    'strategy.symmetry': 'MinimumNodeLabelVerticesNumber',
                 },
-                {
-                    'strategy.partition': 'PaintExactlyOne',
-                },
-                {
-                    'strategy.adjacency': 'AdjacentsLeqColor',
-                },
-                {
-                    'strategy.adjacency': 'AdjacentsLeqOne',
-                },
-                {
-                    'model.adjacentsNeighbourhood.useCliqueCover': 'false'
-                },
-                {
-                    'strategy.adjacency': 'AdjacentsPartitionLeqColor',
-                },
-                {
-                    'strategy.symmetry': 'None',
-                },
-                {
-                    'strategy.symmetry': 'VerticesNumber',
-                },
-                {
-                    'strategy.symmetry': 'MinimumNodeLabel',
-                },
-                {
-                    'strategy.colorBound': 'UpperNodesSum',
-                },
-                {
-                    'strategy.colorBound': 'UpperNodesSumLowerSum',
-                },
-                {
-                    'strategy.colorBound': 'UpperNodesSumLowerSumPartition',
-                },
-                {
-                    'strategy.objective': 'Linear',
-                },
-                {
-                    'strategy.objective': 'LinearReverse',
-                },
-                {
-                    'strategy.objective': 'Geometric',
-                },
+                
             ], 
             
                 files = files,
                 dirs = [ ".\\..\\data\\" ],
+                runid = '20100623215001',
+                initial = 600
             )
     
