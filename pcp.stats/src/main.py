a71e88da60aefbcad427cf3c4919884b40fd51d0
spalladino <devnull@localhost>
2010-07-02 19:05:30 -0300

    Stats

--- pcp.stats/src/main.py
+++ pcp.stats/src/main.py
@@ -12,7 +12,7 @@ def simpletable(p):
                     "branch.prios.enabled",
                     "primal.enabled"], None, {'run.folder': '.\\..\\data\\rand075\\'})
     
-def latextable(p):    
+def model_latextable(p):    
         p.process(
                 ids=[metrics.FileName()],
                 datas=['solution.gap','solution.time'], 
@@ -22,15 +22,23 @@ def latextable(p):
                 aggr= processor.avg
                     )
 
+def dsatur_latextable(p):    
+        p.process(
+                ids=[metrics.FileName()],
+                datas=['solution.chi','solution.time','solution.found'], 
+                series=['strategy.coloring', 'dsatur.partition.weight.size', 'dsatur.partition.weight.colorCount', 'dsatur.partition.weight.uncolored'],
+                datafilter= None,
+                runfilter= None,
+                aggr= processor.avg
+                )
+
 
 if __name__ == '__main__':
-    p = LatexProcessor('20100629MODELS')
-    #p.summary()
-    latextable(p)
+    p = LatexProcessor('20100701DSATURS1M')
+    dsatur_latextable(p)
     #p.graphprops("graph.nodes", "solution.time", "nodes-time.png")
     #p.graphprops("graph.edges", "solution.time", "edges-time.png")
     #p.graphprops("graph.partitions", "solution.time", "parts-time.png")
     #p.graph("graph.nodes", "solution.time", series= ["holes.maxPerColor"], fname= "nodestime.png")
-    
     #p.graph("graph.nodes", metrics.cutcount("Holes"), series= ["holes.maxPerColor"])
 
